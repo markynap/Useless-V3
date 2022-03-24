761ce0e1e62212549761b28d357fb5f8599fbdd5
markynap <markynap@users.noreply.github.com>
2022-03-24 02:12:47 -0400

    Added Furnace Database

--- Furnace.sol
+++ Furnace.sol
@@ -1,5 +1,5 @@
+//SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
-// SPDX-License-Identifier: Unlicensed
 
 import "./IERC20.sol";
 import "./Address.sol";
---
+++ FurnaceDB.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.4;
+import "./Ownable.sol";
+contract FurnaceDatabase is Ownable {
+
+    uint public pullLiquidityRange;
+    uint public buyAndBurnRange;
+    uint public reverseSALRange;
+    event SetRanges(uint pullLiquidityRange_, uint buyAndBurnRange_, uint reverseSALRange_);
+
+    function SetRanges(uint pullLiquidityRange_, uint buyAndBurnRange_, uint reverseSALRange_) external onlyOwner {
+        pullLiquidityRange = pullLiquidityRange_;
+        buyAndBurnRange = buyAndBurnRange_;
+        reverseSALRange = reverseSALRange_;
+        emit SetRanges(pullLiquidityRange_, buyAndBurnRange_, reverseSALRange_);
+    }
+}
